a55ef99893e25e45227bcf1ca5fc112e65caca8a
injedm <injedm@gmail.com>
2017-12-12 18:28:26 +0100

    Added tests for OutputWriterImpl

--- src/test/java/fr/simplifia/output/impl/SmpOutputWriterImplTest.java
+++ src/test/java/fr/simplifia/output/impl/SmpOutputWriterImplTest.java
@@ -1,13 +1,15 @@
 package fr.simplifia.output.impl;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.mockito.Mockito;
-import org.testng.annotations.Test;
 
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by simplifia on 18/09/17.
@@ -15,25 +17,39 @@ import static org.mockito.Mockito.when;
 public class SmpOutputWriterImplTest {
 
     private PrintStream stream;
-
+    private SmpOutputWriterImpl smpOutputWriterImpl;
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     public SmpOutputWriterImplTest(){
         stream = Mockito.mock(PrintStream.class);
-       doNothing().when(stream).println(anyString());
+        smpOutputWriterImpl = new SmpOutputWriterImpl(stream);
     }
 
-    //TODO
     @Test
     public void testPrint() throws Exception {
+    	String input=anyString();
+        smpOutputWriterImpl.print(input);
+    	 String content = new String(outContent.toByteArray(), StandardCharsets.UTF_8);
+         stream.close();
+        Assert.assertEquals(input, content.toString());
     }
 
-    //TODO
     @Test
     public void testPrintEmpty() throws Exception {
+    	String input="";
+        smpOutputWriterImpl.print(input);
+    	 String content = new String(outContent.toByteArray(), StandardCharsets.UTF_8);
+         stream.close();
+        Assert.assertEquals(input, content.toString());
     }
 
-    //TODO
     @Test
     public void testPrintNull() throws Exception {
+    	String input=null;
+        smpOutputWriterImpl.print(input);
+    	 String content = new String(outContent.toByteArray(), StandardCharsets.UTF_8);
+         stream.close();
+        assertEquals("", content.toString());
+        // System.out.println(content.toString());
     }
 
 }
